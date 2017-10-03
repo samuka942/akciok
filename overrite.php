92c392b62ea1cd0888f43326fe8f57d18f9b59c9
samuka942 <samu.gyorgy@mndsblog.com>
2017-10-03 17:52:36 +0200

    változók helyes használata, és megfelelő konstruktor

--- overrite.php
+++ overrite.php
@@ -1,7 +1,8 @@
 <?php
 
-class file_overrite(){
+class file_overrite{
 	
+	public $f;
 	private $file_name;
 	private $hatarido;
 	private $eleje;
@@ -9,23 +10,27 @@ class file_overrite(){
 	private $athenba;
 	private $athenbol;
 	
-	public function file_setter($file_name){
-		$f = file_get_contents($file_name);
+	public function __construct($File_name){
+		$this->file_name = $File_name;
+	}
+	
+	public function file_setter(){
+		$this->f = file_get_contents($this->file_name);
 	}
 	
 	
 	public function Hatarido_Setter($hatarido){
-		$f = str_replace("_hatarido_",$hatarido,$f);
+		$this->f = str_replace("_hatarido_",$hatarido,$this->f);
 	}
 	
 	public function Utazasi_ido_setter($eleje,$vege){
-		$f = str_replace("_ido_eleje_",$eleje,$f);
-		$f = str_replace("_ido_vege_",$vege,$f);
+		$this->f = str_replace("_ido_eleje_",$eleje,$this->f);
+		$this->f = str_replace("_ido_vege_",$vege,$this->f);
 	}
 	
 	public function Athen_arak_Setter($athenba="€19",$athenbol="€41"){
-		$f = str_replace("_athenba_",$athenba,$f);
-		$f = str_replace("_athenbol_",$athenbol,$f);
+		$this->f = str_replace("_athenba_",$athenba,$this->f);
+		$this->f = str_replace("_athenbol_",$athenbol,$this->f);
 	}
 	
 }
