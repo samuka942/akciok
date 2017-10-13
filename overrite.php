d0ec93e18bf99335d63399e91a5307ef4f2eb7ab
samuka942 <samu.gyorgy@mndsblog.com>
2017-10-13 08:14:34 +0200

    kezelő osztály

--- overrite.php
+++ overrite.php
@@ -1,5 +1,8 @@
 <?php
 
+require '../phpmailer/class.phpmailer.php';
+require '../phpmailer/class.smtp.php';
+
 class file_overrite{
 	
 	public $f;
@@ -7,8 +10,10 @@ class file_overrite{
 	private $hatarido;
 	private $eleje;
 	private $vege;
-	private $athenba;
-	private $athenbol;
+	private $idozona;
+	private $kivetelek;
+	private $honnan;
+	private $ar;
 	
 	public function __construct($File_name){
 		$this->file_name = $File_name;
@@ -28,19 +33,69 @@ class file_overrite{
 		$this->f = str_replace("_ido_vege_",$vege,$this->f);
 	}
 	
-	public function Athen_arak_Setter($athenba,$athenbol){
-		$this->f = str_replace("_athenba_",$athenba,$this->f);
-		$this->f = str_replace("_athenbol_",$athenbol,$this->f);
+	public function Idozona_setter($idozona){
+		$this->f = str_replace("_idozona_",$idozona,$this->f);
 	}
 	
 	public function article_saver(){
 		$saver_file = substr($this->file_name,0,stripos($this->file_name,"."))."_kesz.txt";
 		fopen($saver_file,"w");
 		if(file_put_contents($saver_file,$this->f)){
-			echo "A mentés sikeres!";
+			echo "A cikk törzse elkészült!";
 		}
 	}
 	
+	/*public function email_sender(){
+		$error;
+	$mail = new PHPMailer();  // create a new object
+	$mail->CharSet = 'UTF-8';
+	$mail->IsSMTP(); // enable SMTP
+	$mail->SMTPDebug = 0;  // debugging: 1 = errors and messages, 2 = messages only
+	$mail->SMTPAuth = true;  // authentication enabled
+	$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for GMail
+	$mail->Host = 'smtp.gmail.com';
+	$mail->Port = 465; //587 465
+	$mail->Username = "samu942@gmail.com";  
+	$mail->Password = "KurtaKoz@11";           
+	$mail->SetFrom("samu.gyorgy@mndsblog.com", "Samu György");
+	$mail->Subject = "Akciók";
+	$mail->Body = "Az Ön által készített sablon kitöltése sikeres!\nA mellékletből le tudja tölteni!\nÜdvözlettel:\nSamu György";
+	$mail->AddAddress("szutor.mark@mndsblog.com");
+	$mail->addAttachment($saver_file = substr($this->file_name,0,stripos($this->file_name,"."))."_kesz.txt");
+	if(!$mail->Send()) {
+		$error = 'Mail error: '.$mail->ErrorInfo; 
+		return false;
+		} else {
+		$error = 'Message sent!';
+		return true;
+		}
+	}*/
+	
+	public function kivetel_setter($kivetel){
+		$this->f = str_replace("_kivetelek_",$kivetel,$this->f);
+	}
+	
+	public function varos_setter($honnan){
+		$this->f = str_replace("_honnan_",$honnan,$this->f);
+	}
+	
+	public function szazalek_setter($szazalek){
+		$this->f = str_replace("_szazalek_",$szazalek,$this->f);
+	}
+	
+	public function kezi_setter($meret){
+		$this->f = str_replace("_kezi_",$meret,$this->f);
+	}
+	
+	public function ar_setter($ar){
+		$this->f = str_replace("_ar_",$ar,$this->f);
+	}
+	
+	public function kupon_setter($kupon){
+		$this->f = str_replace("_kupon_",$kupon,$this->f);
+	}
+	
 }
+
 
 ?>
